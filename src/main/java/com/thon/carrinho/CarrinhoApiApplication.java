b3984e524609ff7a99559966d04ce13a2941bbaf
THONWELLING <thonwellingdani13@gmail.com>
2022-10-14 00:33:16 -0300

    data base configuration

--- src/main/java/com/thon/carrinho/CarrinhoApiApplication.java
+++ src/main/java/com/thon/carrinho/CarrinhoApiApplication.java
@@ -8,6 +8,9 @@ public class CarrinhoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarrinhoApiApplication.class, args);
+
+		System.out.println("system is running at the port 8080 ===>");
 	}
+
 
 }
