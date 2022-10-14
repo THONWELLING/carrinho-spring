03317699df730d24c37cae0f3eb5a12b6fc91531
THONWELLING <thonwellingdani13@gmail.com>
2022-10-14 11:23:04 -0300

    Create class Product

--- src/main/java/com/thon/carrinho/model/Product.java
+++ src/main/java/com/thon/carrinho/model/Product.java
@@ -1,4 +1,32 @@
 package com.thon.carrinho.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", })
+@NoArgsConstructor
 public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String name;
+  private double unityValue;
+
+  @Builder.Default // sempre que criarmos um produto estará setado que está disponível por padrão
+  private Boolean available = true;
+
+  @ManyToOne // Aqui etamos dizendo que muitos produtos podm pertencer à 1 restaurante
+  @JsonIgnore
+  private Restaurant restaurant;
 }
