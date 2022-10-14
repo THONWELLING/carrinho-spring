1a04a68a5376d943205783c0d8c63a109b9fce09
THONWELLING <thonwellingdani13@gmail.com>
2022-10-13 23:30:02 -0300

    Project created

---
+++ src/test/java/com/thon/carrinho/CarrinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thon.carrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
