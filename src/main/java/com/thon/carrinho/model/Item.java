0e25744e8128844ab3a06e8ee5bd3cd24c2065fb
THONWELLING <thonwellingdani13@gmail.com>
2022-10-14 11:14:26 -0300

    Create class Item

--- src/main/java/com/thon/carrinho/model/Item.java
+++ src/main/java/com/thon/carrinho/model/Item.java
@@ -1,4 +1,36 @@
 package com.thon.carrinho.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", })
+@NoArgsConstructor
+
+/*
+* Essa classe será uma tabela no banco de dados.
+* Essa tabela terá relacionamento com outras 2 tabelas, Product e Carrinho*/
 public class Item {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+
+  @OneToOne  // representando que cada Item só pode ter um produto
+  private Product product;
+  private int quantity;
+
+  @ManyToOne  // aqui representando que o carrinho poderá ter muitos itens/produtos
+  @JsonIgnore
+  private Carrinho carrinho;
 }
