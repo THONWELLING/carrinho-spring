d3958fe13282035f6da5dbab57a49e7cf604f730
THONWELLING <thonwellingdani13@gmail.com>
2022-10-18 23:22:08 -0300

    implementando addItemOnCarrinho

--- src/main/java/com/thon/carrinho/service/impl/CarrinhoServiceImpl.java
+++ src/main/java/com/thon/carrinho/service/impl/CarrinhoServiceImpl.java
@@ -3,19 +3,69 @@ package com.thon.carrinho.service.impl;
 import com.thon.carrinho.enumeration.PaymentForm;
 import com.thon.carrinho.model.Carrinho;
 import com.thon.carrinho.model.Item;
+import com.thon.carrinho.model.Product;
+import com.thon.carrinho.model.Restaurant;
 import com.thon.carrinho.repository.CarrinhoRepository;
+import com.thon.carrinho.repository.ItemRepository;
+import com.thon.carrinho.repository.ProductRepository;
 import com.thon.carrinho.resouce.dto.ItemDto;
 import com.thon.carrinho.service.CarrinhoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CarrinhoServiceImpl implements CarrinhoService {
   private final CarrinhoRepository carrinhoRepository;
+  private final ProductRepository productRepository;
+  private final ItemRepository itemRepository;
+
   @Override
   public Item addItemOnCarrinho(ItemDto itemDto) {
-    return null;
+    //Pegando o carrinho
+    Carrinho carrinho = showCarrinho(itemDto.getCarrinhoId());
+
+    //Verificando se o carrinho está fechado
+    if (carrinho.isClosed()) {
+      throw new RuntimeException("Carrinho está fechado.");
+    }
+
+    //Criar um item
+    Item itemToAdd = Item.builder()
+        .quantity(itemDto.getQuantity())
+        .carrinho(carrinho)
+        .product(productRepository.findById(itemDto.getProductId()).orElseThrow(() -> {
+          throw new RuntimeException("Esse Produto Não existe!!!");
+        }))
+        .build();
+
+    /*
+    Criando a regra de negócio como a do ifood:
+    * Cada carrinho só pode ter itens do mesmo restaurante para inserir itens de outro restaurante
+    * precisa de um novo pedido /carrinho
+    */
+
+    List<Item> carrinhoItems = carrinho.getItems(); // Pegando a lista de itens do carrinho
+
+    // Se a lista está vazia simplesmente adiciono
+    if (carrinhoItems.isEmpty()) {
+      carrinhoItems.add(itemToAdd);
+    } else {
+      /*
+      * pegando o 1º elemento da lista.associando qual é o produto e qual é o restaurante e atribuindo a uma variavel
+       */
+      Restaurant currentRestaurant = carrinhoItems.get(0).getProduct().getRestaurant();
+      Restaurant restaurantOfNewProduct = itemToAdd.getProduct().getRestaurant(); //Pegando o restaurante do produto que quero adicionar
+      if (currentRestaurant.equals(restaurantOfNewProduct)) {
+        carrinhoItems.add(itemToAdd);
+      } else {
+        throw new RuntimeException("Não é possível adicionar itens de restaurantes diferentes.");
+      }
+    }
+    carrinhoRepository.save(carrinho);
+    return itemRepository.save(itemToAdd);
   }
 
   @Override
